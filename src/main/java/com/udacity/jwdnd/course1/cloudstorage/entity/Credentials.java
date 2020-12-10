d7bd963a69368ca673e495bf9647af2b9e88f366
Berk Yasar <berkyasar@Berks-MacBook-Pro.local>
2020-12-10 23:19:50 +0300

    Lombok dependency added and login implemented.

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/entity/Credentials.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/entity/Credentials.java
@@ -1,5 +1,8 @@
 package com.udacity.jwdnd.course1.cloudstorage.entity;
 
+import lombok.Data;
+
+@Data
 public class Credentials {
     private Integer credentialid;
     private String url;
@@ -8,51 +11,4 @@ public class Credentials {
     private String password;
     private Integer userid;
 
-    public int getCredentialid() {
-        return credentialid;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getKey() {
-        return key;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public int getUserid() {
-        return userid;
-    }
-
-    public void setCredentialid(int credentialid) {
-        this.credentialid = credentialid;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setKey(String key) {
-        this.key = key;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setUserid(int userid) {
-        this.userid = userid;
-    }
 }
--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/entity/Notes.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/entity/Notes.java
@@ -1,40 +1,11 @@
 package com.udacity.jwdnd.course1.cloudstorage.entity;
 
+import lombok.Data;
+
+@Data
 public class Notes {
     private Integer noteid;
     private String notetitle;
     private String notedescription;
     private Integer userid;
-
-    public int getNoteid() {
-        return noteid;
-    }
-
-    public String getNotetitle() {
-        return notetitle;
-    }
-
-    public String getNotedescription() {
-        return notedescription;
-    }
-
-    public int getUserid() {
-        return userid;
-    }
-
-    public void setNoteid(int noteid) {
-        this.noteid = noteid;
-    }
-
-    public void setNotetitle(String notetitle) {
-        this.notetitle = notetitle;
-    }
-
-    public void setNotedescription(String notedescription) {
-        this.notedescription = notedescription;
-    }
-
-    public void setUserid(int userid) {
-        this.userid = userid;
-    }
 }
