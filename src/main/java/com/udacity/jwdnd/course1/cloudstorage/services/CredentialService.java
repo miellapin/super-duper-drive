24f7b29e9394fe105a446019f34e7a8e5367411f
Berk Yasar <berkyaar44@gmail.com>
2021-02-13 20:37:27 +0300

    Credentials hashed password fix

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/CredentialService.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/CredentialService.java
@@ -1,22 +1,28 @@
 package com.udacity.jwdnd.course1.cloudstorage.services;
 
 import com.udacity.jwdnd.course1.cloudstorage.entity.Credentials;
-import com.udacity.jwdnd.course1.cloudstorage.entity.Notes;
 import com.udacity.jwdnd.course1.cloudstorage.mapper.CredentialMapper;
 import org.springframework.stereotype.Service;
 
+import java.util.Base64;
 import java.util.List;
 
 @Service
 public class CredentialService {
 
     private final CredentialMapper credentialMapper;
+    private final HashService hashService;
 
-    public CredentialService(CredentialMapper credentialMapper) {
+    public CredentialService(CredentialMapper credentialMapper, HashService hashService) {
         this.credentialMapper = credentialMapper;
+        this.hashService = hashService;
     }
 
     public void addCredential(Credentials credentials) {
+        byte[] salt = new byte[16];
+        String encodedSalt = Base64.getEncoder().encodeToString(salt);
+        String hashedPassword = hashService.getHashedValue(credentials.getPassword(), encodedSalt);
+        credentials.setKey(hashedPassword);
         credentialMapper.addCredential(credentials);
     }
 
